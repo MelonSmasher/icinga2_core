2840bffcb36f30119b6b8ca80fb1372828f4c229
Alex Markessinis <markea@sage.edu>
2017-09-28 09:45:18 -0400

    fixed error

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'MIT'
 description 'Installs icinga2 core'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 chef_version '>= 12' if respond_to?(:chef_version)
-version '0.1.0'
+version '0.1.1'
 
 issues_url 'https://github.com/MelonSmasher/icinga2_core/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/icinga2_core' if respond_to?(:source_url)
